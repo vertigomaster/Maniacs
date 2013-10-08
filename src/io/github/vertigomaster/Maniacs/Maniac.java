95acc16f88406f21028f70aa74a0e60cca15fd3a
vertigomaster <janoel777@gmail.com>
2013-10-08 17:50:49 -0500

    File editing from github
    Tested the file editing from github

--- src/io/github/vertigomaster/Maniacs/Maniac.java
+++ src/io/github/vertigomaster/Maniacs/Maniac.java
@@ -2,6 +2,17 @@ package io.github.vertigomaster.Maniacs;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Maniac  extends JavaPlugin{
-
+public final class Maniac  extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		getLogger().info("onEnable has been invoked! :D");
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		getLogger().info("onDisable has been invoked! :D");
+	}
 }
